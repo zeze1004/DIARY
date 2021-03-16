6c49aa07218e0d7760f1619bb8878703b8b96c9f
dewy.i <dewy.i@kakaocorp.com>
2021-03-16 22:16:35 +0900

    Use storage

---
+++ whale-extension/src/storage.ts
@@ -0,0 +1,28 @@
+async function setToStorage(key: string, value: any): Promise<void> {
+  return new Promise((resolve, reject) => {
+    chrome.storage.local.set({[key]: value}, function() {
+      if (chrome.runtime.lastError) {
+        console.log(chrome.runtime.lastError);
+        reject();
+      }
+      resolve();
+    });
+  });
+}
+
+async function getFromStorage(key: string): Promise<any> {
+  return new Promise((resolve, reject) => {
+    chrome.storage.local.get(key, function (result) {
+      if (chrome.runtime.lastError) {
+        console.log(chrome.runtime.lastError);
+        reject();
+      }
+      resolve(result[key]);
+    });
+  });
+}
+
+export {
+  setToStorage,
+  getFromStorage,
+}
