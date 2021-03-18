08b42596f122753e2e8aa0279bab15219004ea59
zeze1004 <thwjd2717@naver.com>
2021-03-18 19:11:25 +0900

    user table

---
+++ src/main/java/com/ssonobackend/diarybackend/domain/Member.java
@@ -0,0 +1,20 @@
+package com.ssonobackend.diarybackend.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+@Setter
+public class Member {
+    @Id
+    @GeneratedValue
+    @Column
+    private Long id;
+    private String name;
+}
---
+++ src/test/java/com/ssonobackend/diarybackend/service/MemberServiceTest.java
@@ -0,0 +1,38 @@
+package com.ssonobackend.diarybackend.service;
+
+import com.ssonobackend.diarybackend.domain.Member;
+import com.ssonobackend.diarybackend.repository.MemberRepository;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Transactional
+class MemberServiceTest {
+    @Autowired
+    MemberService memberService;
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Test
+    @Rollback(false)
+    public void 회원가입() throws Exception {
+        // given
+        Member member = new Member();
+        member.setName("zeze");
+
+        // when
+        Long savedId = memberService.join(member);
+
+        // then
+        assertEquals(member, memberRepository.findOne(savedId)); // 가입한 회원이랑 찾은 회원이랑 같으면 정상 가입(현재 db에 가입한 회원이 없음)
+    }
+}
