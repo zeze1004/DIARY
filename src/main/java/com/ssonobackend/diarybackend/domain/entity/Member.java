2b6d40b33d02ed54e2208b0bf7edc3ac104289b1
zeze1004 <thwjd2717@naver.com>
2021-05-13 22:14:29 +0900

    rds connecting

--- src/main/java/com/ssonobackend/diarybackend/domain/entity/Member.java
+++ src/main/java/com/ssonobackend/diarybackend/domain/entity/Member.java
@@ -32,7 +32,7 @@ public class Member implements UserDetails {
 
 
     @Enumerated(EnumType.STRING)  // JPA로 데이터베이스로 저장할 때 Enum 값을 어떤 형태로 저장할지 결정
-    @Column(nullable = false)     // 기본적으로 int로 된 숫자가 저장됨, 숫자로하면 의미를 알 수 없어 문자열로 수정
+    @Column(nullable = true)     // 기본적으로 int로 된 숫자가 저장됨, 숫자로하면 의미를 알 수 없어 문자열로 수정
     private Role role;
 
     @OneToMany(mappedBy = "member")
